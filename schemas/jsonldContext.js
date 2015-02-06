134959c177f25f5337e844835b710a75c4cba826
Dave Longley <dlongley@digitalbazaar.com>
2015-02-06 16:19:10 -0500

    Require context (url) parameter.

--- schemas/jsonldContext.js
+++ schemas/jsonldContext.js
@@ -2,22 +2,20 @@
  * Copyright (c) 2012-2015 Digital Bazaar, Inc. All rights reserved.
  */
 var tools = require('bedrock').tools;
-var config = require('bedrock').config;
 
-var schema = {
-  required: true,
-  title: 'JSON-LD context',
-  description: 'A JSON-LD Context',
-  type: [{
-    type: 'string',
-    enum: [config.constants.CONTEXT_URL]
-  }, {
-    type: 'object'
-    // FIXME: improve context object validator
-  }]
-};
-
-module.exports = function(extend) {
+module.exports = function(context, extend) {
+  var schema = {
+    required: true,
+    title: 'JSON-LD context',
+    description: 'A JSON-LD Context',
+    type: [{
+      type: 'string',
+      enum: [context]
+    }, {
+      type: 'object'
+      // FIXME: improve context object validator
+    }]
+  };
   if(extend) {
     return tools.extend(true, tools.clone(schema), extend);
   }
