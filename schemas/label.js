2bc5426a414abfc759cbe492cb3c8f76c8ffe058
Dave Longley <dlongley@digitalbazaar.com>
2015-03-20 10:38:29 -0400

    Allow double quotes in labels/titles; allow labels to be 200 chars.

--- schemas/label.js
+++ schemas/label.js
@@ -8,12 +8,12 @@ var schema = {
   title: 'Label',
   description: 'A short, descriptive label.',
   type: 'string',
-  pattern: '^[-a-zA-Z0-9~!$%^&*\\(\\)_=+\\. ]*$',
+  pattern: '^[-a-zA-Z0-9~`!@#$%^&*\\(\\)\\[\\]{}<>_=+\\\\|:;\'"\\.,/? ]*$',
   minLength: 1,
-  maxLength: 32,
+  maxLength: 200,
   errors: {
     invalid: 'The label contains invalid characters or is not between ' +
-      '1 and 32 characters in length.',
+      '1 and 200 characters in length.',
     missing: 'Please enter a label.'
   }
 };
