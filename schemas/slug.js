9ca15a15d557a4e69afdde3a66527489a5d8093e
Manu Sporny <msporny@digitalbazaar.com>
2017-03-05 15:31:12 -0500

    Expand slugs to allow UUIDs to be used.

--- schemas/slug.js
+++ schemas/slug.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2015 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2012-2017 Digital Bazaar, Inc. All rights reserved.
  */
 var bedrock = require('bedrock');
 
@@ -8,13 +8,14 @@ var schema = {
   title: 'Slug',
   description: 'A short identifier within a URL.',
   type: 'string',
-  pattern: '^[a-z][-a-z0-9~_\\.]*$',
+  pattern: '^[a-z0-9][-a-z0-9~_\\.]*$',
   minLength: 3,
-  maxLength: 32,
+  maxLength: 40,
   errors: {
-    invalid: 'The slug must start with a letter, contain only lowercase ' +
+    invalid: 
+      'The slug must start with a letter or number, contain only lowercase ' +
       'letters, numbers, hyphens, periods, underscores, and tildes. It must ' +
-      'between 3 and 32 characters in length.',
+      'between 3 and 40 characters in length.',
     missing: 'Please enter a slug.'
   }
 };
