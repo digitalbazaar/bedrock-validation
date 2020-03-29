8e12cc0936ad515aa614dbf02a8429d0faf88c25
Ganesh Annan <gannan@digitalbazaar.com>
2020-03-28 22:47:07 -0400

    Increase character length of labels per RFC 1034.

--- schemas/email.js
+++ schemas/email.js
@@ -3,11 +3,13 @@
  */
 const bedrock = require('bedrock');
 
+// RFC 1034 - All labels have a max length of 63 octets.
+// https://tools.ietf.org/html/rfc1034#section-3.1
 const schema = {
   title: 'Email',
   description: 'An email address.',
   type: 'string',
-  pattern: "^[-a-zA-Z0-9~!$%^&*_=+}{\\'?]+(\\.[-a-zA-Z0-9~!$%^&*_=+}{\\'?]+)*@(((([a-zA-Z0-9]{1}[a-zA-Z0-9\\-]{0,62}[a-zA-Z0-9]{1})|[a-zA-Z])\\.)+[a-zA-Z]{2,6})$",
+  pattern: "^[-a-zA-Z0-9~!$%^&*_=+}{\\'?]+(\\.[-a-zA-Z0-9~!$%^&*_=+}{\\'?]+)*@(((([a-zA-Z0-9]{1}[a-zA-Z0-9\\-]{0,63}[a-zA-Z0-9]{1})|[a-zA-Z])\\.)+[a-zA-Z]{2,63})$",
   minLength: 1,
   maxLength: 100,
   errors: {
@@ -20,7 +22,7 @@ module.exports = function(extend, options) {
   if(options && options.lowerCaseOnly) {
     extend = extend || {};
     if(!('pattern' in extend)) {
-      extend.pattern = "^[-a-z0-9~!$%^&*_=+}{\\'?]+(\\.[-a-z0-9~!$%^&*_=+}{\\'?]+)*@(((([a-z0-9]{1}[a-z0-9\\-]{0,62}[a-z0-9]{1})|[a-z])\\.)+[a-z]{2,6})$";
+      extend.pattern = "^[-a-z0-9~!$%^&*_=+}{\\'?]+(\\.[-a-z0-9~!$%^&*_=+}{\\'?]+)*@(((([a-z0-9]{1}[a-z0-9\\-]{0,63}[a-z0-9]{1})|[a-z])\\.)+[a-z]{2,63})$";
     }
   }
   if(extend) {
