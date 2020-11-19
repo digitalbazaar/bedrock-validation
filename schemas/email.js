5a74a491fc2e861ff470fb507c5ac9f0d6983ff3
Matthew Collier <mcollier@digitalbazaar.com>
2020-11-18 21:24:05 -0500

    Fix linting issues.

--- schemas/email.js
+++ schemas/email.js
@@ -9,7 +9,8 @@ const schema = {
   title: 'Email',
   description: 'An email address.',
   type: 'string',
-  pattern: "^[-a-zA-Z0-9~!$%^&*_=+}{\\'?]+(\\.[-a-zA-Z0-9~!$%^&*_=+}{\\'?]+)*@(((([a-zA-Z0-9]{1}[a-zA-Z0-9\\-]{0,63}[a-zA-Z0-9]{1})|[a-zA-Z])\\.)+[a-zA-Z]{2,63})$",
+  // eslint-disable-next-line max-len
+  pattern: '^[-a-zA-Z0-9~!$%^&*_=+}{\\\'?]+(\\.[-a-zA-Z0-9~!$%^&*_=+}{\\\'?]+)*@(((([a-zA-Z0-9]{1}[a-zA-Z0-9\\-]{0,63}[a-zA-Z0-9]{1})|[a-zA-Z])\\.)+[a-zA-Z]{2,63})$',
   minLength: 1,
   maxLength: 100,
   errors: {
@@ -22,7 +23,8 @@ module.exports = function(extend, options) {
   if(options && options.lowerCaseOnly) {
     extend = extend || {};
     if(!('pattern' in extend)) {
-      extend.pattern = "^[-a-z0-9~!$%^&*_=+}{\\'?]+(\\.[-a-z0-9~!$%^&*_=+}{\\'?]+)*@(((([a-z0-9]{1}[a-z0-9\\-]{0,63}[a-z0-9]{1})|[a-z])\\.)+[a-z]{2,63})$";
+      // eslint-disable-next-line max-len
+      extend.pattern = '^[-a-z0-9~!$%^&*_=+}{\\\'?]+(\\.[-a-z0-9~!$%^&*_=+}{\\\'?]+)*@(((([a-z0-9]{1}[a-z0-9\\-]{0,63}[a-z0-9]{1})|[a-z])\\.)+[a-z]{2,63})$';
     }
   }
   if(extend) {
