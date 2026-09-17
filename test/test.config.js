9054eab7f5374e3122461d8370daabb1736bb2c9
David I. Lehn <dlehn@digitalbazaar.com>
2026-09-16 22:49:30 -0400

    Use `import.meta.dirname`.

--- test/test.config.js
+++ test/test.config.js
@@ -2,9 +2,6 @@
  * Copyright (c) 2017-2026 Digital Bazaar, Inc.
  */
 import {config} from '@bedrock/core';
-import {fileURLToPath} from 'node:url';
 import path from 'node:path';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-config.mocha.tests.push(path.join(__dirname, 'mocha'));
+config.mocha.tests.push(path.join(import.meta.dirname, 'mocha'));
